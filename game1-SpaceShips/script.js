c7fcc043e100ec93519ccb47a2f0e1cb001c7ed1
Felix Palacios <flpalacios93@gmail.com>
2024-05-06 14:01:19 +0200

    Add projectiles to the player , learning how to position the projectile to shoot from the middle of the player

--- game1-SpaceShips/script.js
+++ game1-SpaceShips/script.js
@@ -6,34 +6,99 @@ class Player {
     // size of the player
     this.width = 50;
     this.height = 100;
-    // mueve el jugador 5 px per animation frame 
+    // mueve el jugador 5 px per animation frame
     this.speed = 5;
 
-    // position of the player 
+    // position of the player
     // se utilizara el game object para tomar
-    // la referencia de la posicion  del jugador 
-   // posiciona al jugador horizontalmente 
+    // la referencia de la posicion  del jugador
+   // posiciona al jugador horizontalmente
    // de izquierda a derecha
     this.x = this.game.width * 0.5 - this.width * 0.5;
 
-    // posiciona al jugador verticalmente de arriba a abajo 
+    // posiciona al jugador verticalmente de arriba a abajo
     this.y = this.game.height - this.height;
 
   }
 
   draw(context) {
   context.fillRect(this.x,this.y, this.width, this.height);
-  
   }
   // actualiza la posicion del jugador osea le permite moverse
   update() {
-    this.x += this.speed;
+    // move left and right
+    if(this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+
+    if(this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+    // move up and down
+   // if(this.game.keys.indexOf('ArrowUp') > -1) this.y -= this.speed;
+
+    //if(this.game.keys.indexOf('ArrowDown') > -1) this.y += this.speed;
+
+    // set limits of the canvas
+    // impide que el jugador salga del canvas de izquierda a derecha
+    //  pero le permite que se mueve hasta la mitad su ancho para poder disparar bien al enemigo
+    // si solo fuera que llegue justo hasta el borde del canva seria
+    // if(this.y < 0) this.y = 0;
+    if(this.x < -this.width * 0.5) this.x = -this.width * 0.5;
+    else if(this.x > this.game.width - this.width ) this.x = this.game.width - this.width;
+    // impide que el jugador salga del canvas de arriba 
+    // if(this.y < 0) this.y = 0;
+    // else if(this.height > this.game.height - this.height) this.height =  this.game.height - this.height;
+  }
+
+  // Este metodo permite despirar cada vez que presionemos la tecla 1
+  // dispara desde la posicion del jugador para ellos tomamos su posicion
+ // al disparar se le anade al ancho del jugador el witdh con 0.5 para que tome justo la mitad
+ // este metodo esta dentro de la clase projectile que le quitaremos el ancho del jugador por 0.5
+ // para que sea la mitad
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if(projectile) projectile.start(this.x + this.width * 0.5, this.y);
+
   }
 
 }
 
 class Projectile {
+constructor() {
+  this.width = 4;
+  this.height= 20;
+  this.x = 0;
+  this.y = 0;
+  this.speed = 20;
+   // es un object pool que no lo vemos cuando este en true
+   // y cunado sea false aparecera
+  this.free = true;
+}
+// permite que se vea el projectile cuando dispara
 
+draw(context) {
+
+  if(!this.free) {
+    context.fillRect(this.x,this.y, this.width, this.height);
+  }
+}
+
+update() {
+  if(!this.free) {
+    this.y -= this.speed;
+    if(this.y < -this.height) this.reset();
+  }
+
+}
+// Recibe las coordinadas del jugador , poruqe lo necesita para cada vez que dispara
+// para que dispare desde el centro es la posicion x del projectile que es el corner izquierdo
+// MENOS el ancho del jugador  por 0.5 
+start(x, y) {
+  this.x = x - this.width * 0.5;
+  this.y = y;
+  this.free = false;
+}
+
+reset() {
+  this.free = true;
+}
 
 }
 
@@ -48,7 +113,12 @@ class Game {
     //  the this inside of parenthesis makes reference to the Game class
     this.player = new Player(this);
     this.keys = [];
+    this.player = new Player(this);
 
+
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
     //event listeners
 
     window.addEventListener('keydown', e => {
@@ -57,26 +127,50 @@ class Game {
       // y utiliza el array Keys que se ira guardando los event que hagamos cuando presionamoas las fechcas del teclado
       // gracias al arrow function
       if(this.keys.indexOf(e.key) === -1) { this.keys.push(e.key) };
+      if(e.key === '1') this.player.shoot();
       // DESCOMENTAR SI QUIERES VER COMO SOLO LO ANADE UNA VEZ EL EVENT DE TECLADO
-     console.log(this.keys);
+     //console.log(this.keys);
     });
 
-    // utilizamos splice to remove elementos existentes en el array , solo removermos uno 
+    // utilizamos splice to remove elementos existentes en el array , solo removermos uno
     // si presiono lo guardo si suelta la tecla lo remueve
 
     window.addEventListener('keyup', e => {
-      //if(this.keys.indexOf(e.key) === -1) { this.keys.push(e.key) };
       const index = this.keys.indexOf(e.key);
       if (index > -1) { this.keys.splice(index, 1)};
       console.log(this.keys);
     });
   }
-  
-  // ejecuta los metodos de Player 
+
+  // ejecuta los metodos de Player
   render(context) {
     // console.log(this.width, this.height);
     this.player.draw(context);
     this.player.update();
+    // obtiene los projectiles  , llama a draw para dibujarlo y update para que se actualice
+    // cada vez que supera la altura del canvas
+    this.projectilesPool.forEach(projectile => {
+      projectile.update();
+      projectile.draw(context);
+    })
+  }
+
+
+  // create projectile object pool
+
+  createProjectiles() {
+    for(let i = 0 ; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+
+
+  // get free projectile object from the pool
+
+  getProjectile() {
+    for(let i =0;i< this.projectilesPool.length; i++) {
+      if(this.projectilesPool[i].free)  return this.projectilesPool[i];
+    }
   }
 }
 
@@ -93,7 +187,7 @@ class Game {
     // esta funcion hace que se repita/actualice la posicion y el movimiento del jugador
     // para ellos utiliza la funcion requestAnimationFrame que hara como un loop itera sobre las propiedades segun usuario
     function animatePlayer() {
-      // limpia el canvas 
+      // limpia el canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       game.render(ctx);
